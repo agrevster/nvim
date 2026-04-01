5b1612bd802acb24187aabb118a06771c71013dd
agrevster <138630000+agrevster@users.noreply.github.com>
2026-04-01 13:32:44 -0500

    Removed code preventing changes being added to clipboard

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,11 +2,11 @@ local keymap = vim.keymap
 
 -- Change text without putting it into the vim register,
 -- see https://stackoverflow.com/q/54255/6064933
-keymap.set("n", "c", '"_c')
-keymap.set("n", "C", '"_C')
-keymap.set("n", "cc", '"_cc')
-keymap.set("x", "c", '"_c')
-keymap.set("x", "p", '"_c<Esc>p')
+-- keymap.set("n", "c", '"_c')
+-- keymap.set("n", "C", '"_C')
+-- keymap.set("n", "cc", '"_cc')
+-- keymap.set("x", "c", '"_c')
+-- keymap.set("x", "p", '"_c<Esc>p')
 
 -- Switch wi*ow
 keymap.set('n', '<C-l>', ':tabnext<CR>', { noremap = true, silent = true })
